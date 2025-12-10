cdf559f12acbe656c9c2c53031205e9598467505
Vaibhavvi <163631801+Vaibhavvi@users.noreply.github.com>
2025-12-10 12:18:46 +0530

    End with this chapter how to make Dynamic routing and how to use ejs (Best experience )

--- PROJECT-SETUP/index.js
+++ PROJECT-SETUP/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
@@ -13,6 +15,18 @@ app.get('/', (req,res) => {
     res.render('main', { title: 'Project Setup', message: 'Welcome to the Project Setup Day 06!' });
 })
 
+app.get('/profile/:username', (req,res) => {
+    const username = req.params.username;
+    res.send("chal rha hai");
+    console.log(username);
+})
+
+app.get('/about/:username/:age', (req,res) => {
+    const username = req.params.username;
+    const age = req.params.age;
+    res.send(username + " " + age);
+    console.log(username + " " + age);
+})
 app.listen(PORT, () => {
     console.log(`Server run on http://localhost:${PORT}`)
 })
